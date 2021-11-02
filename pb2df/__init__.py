078f1e608ca4f460abc32a593f5221f6dc91228f
zhangxianbing <zhangxianbing09@163.com>
2021-11-02 15:48:17 +0800

    chore: bump to 1.0.0

--- pb2df/__init__.py
+++ pb2df/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Xianbing Zhang"""
 __email__ = "zhangxianbing09@163.com"
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 
 
 from google.protobuf.descriptor import Descriptor, FieldDescriptor
